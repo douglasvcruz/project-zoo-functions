061727c6f998e067e1599f4d9d76dcbb6114eb20
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-08 00:35:32 -0300

    8º Requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entries = {
+    senior: 0,
+    adult: 0,
+    child: 0,
+  };
+
+  entries.senior = entrants.filter((entrant) => entrant.age === 50).length;
+  entries.adult = entrants.filter((entrant) => entrant.age === 18).length;
+  entries.child = entrants.filter((entrant) => entrant.age === 5).length;
+
+  return entries;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const func = (countEntrants(entrants));
+  return func.senior * data.prices.senior + func.adult * data.prices.adult
+    + func.child * data.prices.child;
 }
 
 module.exports = { calculateEntry, countEntrants };
