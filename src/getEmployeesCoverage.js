96b8a6cf287870ee1b2444c032f50136701c41af
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-10 03:56:33 -0300

    11º, 12º e 13º Requisito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,51 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { species, employees } = data;
+
+const getSpecies = () => employees.map((employ) => ({
+  id: employ.id,
+  fullName: `${employ.firstName} ${employ.lastName}`,
+  species: species.filter(({ id }) => employ.responsibleFor.includes(id)).map((i) => i.name),
+  locations: species.filter(({ id }) => employ.responsibleFor.includes(id)).map((i) => i.location),
+}));
+
+const getSpeciesId = (param) => {
+  const all = employees.find(({ id }) => id === param.id);
+
+  return {
+    id: param.id,
+    fullName: `${all.firstName} ${all.lastName}`,
+    species: species.filter(({ id }) => all.responsibleFor.includes(id)).map((i) => i.name),
+    locations: species.filter(({ id }) => all.responsibleFor.includes(id)).map((i) => i.location),
+  };
+};
+
+const getSpeciesName = (param) => {
+  const utilize = employees.find((employee) =>
+    employee.lastName === param.name || employee.firstName === param.name);
+  const responsible = employees.find((employee) =>
+    employee.lastName === param.name || employee.firstName === param.name).responsibleFor;
+
+  return {
+    id: utilize.id,
+    fullName: `${utilize.firstName} ${utilize.lastName}`,
+    species: species.filter(({ id }) => responsible.includes(id)).map((i) => i.name),
+    locations: species.filter(({ id }) => responsible.includes(id)).map((i) => i.location),
+  };
+};
+
+const getEmployeesCoverage = (param) => {
+  if (param === undefined || param === null) {
+    return getSpecies();
+  }
+  if (employees.find(({ id }) => id === param.id)) {
+    return getSpeciesId(param);
+  }
+  if (employees.find(({ lastName, firstName }) =>
+    firstName === param.name || lastName === param.name)) {
+    return getSpeciesName(param);
+  }
+  throw new Error('Informações inválidas');
+};
 
 module.exports = getEmployeesCoverage;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,31 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Test: should return correct opening hours', () => {
+    expect(getOpeningHours()).toEqual({ Friday: { close: 8, open: 10 }, Monday: { close: 0, open: 0 }, Saturday: { close: 10, open: 8 }, Sunday: { close: 8, open: 8 }, Thursday: { close: 8, open: 10 }, Tuesday: { close: 6, open: 8 }, Wednesday: { close: 6, open: 8 } });
+  });
+  it('Test: the zoo is open', () => {
+    expect(getOpeningHours('Saturday', '10:00-AM')).toBe('The zoo is open');
+  });
+  it('Test: the zoo is closed - PM', () => {
+    expect(getOpeningHours('Saturday', '10:00-PM')).toBe('The zoo is closed');
+  });
+  it('Test: the zoo is closed - Monday', () => {
+    expect(getOpeningHours('Monday', '10:05-PM')).toBe('The zoo is closed');
+  });
+  it('Error: the hour should represent a number', () => {
+    expect(() => getOpeningHours('Monday', 'test')).toThrow(new Error('The hour should represent a number'));
+  });
+  it('Error: the day must be valid. Example: Monday', () => {
+    expect(() => getOpeningHours('test', 'test-PM')).toThrow(new Error('The day must be valid. Example: Monday'));
+  });
+  it('Error: the minutes must be between 0 and 59', () => {
+    expect(() => getOpeningHours('Friday', '10:69-PM')).toThrow(new Error('The minutes must be between 0 and 59'));
+  });
+  it('Error: the hour must be between 0 and 12', () => {
+    expect(() => getOpeningHours('Friday', '26:00-PM')).toThrow(new Error('The hour must be between 0 and 12'));
+  });
+  it('Error: the abbreviation must be \'AM\' or \'PM\'', () => {
+    expect(() => getOpeningHours('Friday', '26:00-TM')).toThrow(new Error('The abbreviation must be \'AM\' or \'PM\''));
+  });
+});
