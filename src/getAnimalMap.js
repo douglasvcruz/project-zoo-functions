8fd2baaa7812f0640cce6594199e5b22b5e991cc
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-23 16:36:17 -0300

    9º Requisito e 14º Requisito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,85 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const locals = ['NE', 'NW', 'SE', 'SW'];
+
+function mapSex(options) {
+  const obj = {};
+  locals.forEach((local) => {
+    obj[local] = species
+      .filter((specie) => specie.location === local)
+      .map((specie) => ({
+        [specie.name]: specie.residents
+          .filter((animal) => animal.sex === options.sex)
+          .map((animal) => animal.name),
+      }));
+  });
+  return obj;
+}
+
+function mapSexSort(options) {
+  const obj = {};
+  locals.forEach((local) => {
+    obj[local] = species
+      .filter((specie) => specie.location === local)
+      .map((specie) => ({
+        [specie.name]: specie.residents
+          .filter((animal) => animal.sex === options.sex)
+          .map((animal) => animal.name)
+          .sort(),
+      }));
+  });
+  return obj;
+}
+
+function mapSort() {
+  const obj = {};
+  locals.forEach((local) => {
+    obj[local] = species
+      .filter((specie) => specie.location === local)
+      .map((specie) => ({
+        [specie.name]: specie.residents.map((animal) => animal.name).sort(),
+      }));
+  });
+  return obj;
+}
+
+function mapFilter(options) {
+  const obj = {};
+  locals.forEach((local) => {
+    obj[local] = species
+      .filter((specie) => specie.location === local)
+      .map((specie) => ({
+        [specie.name]: specie.residents.map((animal) => animal.name),
+      }));
+  });
+  if (options.sorted === true && options.sex !== undefined) {
+    return mapSexSort(options);
+  }
+  if (options.sex) {
+    return mapSex(options);
+  }
+  if (options.sorted === true) {
+    return mapSort();
+  }
+  return obj;
+}
+
+function mapAnimal() {
+  const obj = {};
+  locals.forEach((local) => {
+    obj[local] = species
+      .filter((specie) => specie.location === local)
+      .map(({ name }) => name);
+  });
+  return obj;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || options.includeNames === undefined) {
+    return mapAnimal();
+  }
+  return mapFilter(options);
 }
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const days = Object.keys(data.hours);
+  const { hours, species } = data;
+  const obj = {};
+  days.forEach((day) => {
+    if (day === 'Monday') {
+      obj[day] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    } else {
+      obj[day] = { officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+        exhibition: species.filter((sp) => sp.availability.includes(day)).map(({ name }) => name) };
+    }
+  });
+  if (days.includes(scheduleTarget)) return { [scheduleTarget]: obj[scheduleTarget] };
+
+  if (species.map((specie) => specie.name).includes(scheduleTarget)) {
+    return species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+  return obj;
 }
 
 module.exports = getSchedule;
